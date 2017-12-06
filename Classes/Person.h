2af91768bc616ef2423939d4306291d375c808db
guojun <guojunx@jumei.com>
2017-12-06 19:58:47 +0800

    first commit

---
+++ Classes/Person.h
@@ -0,0 +1,17 @@
+//
+//  Person.h
+//  PodDemo
+//
+//  Created by guojunxu on 2017/12/6.
+//  Copyright © 2017年 guojunxu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@property (nonatomic, copy) NSString *firstName;
+@property (nonatomic, copy) NSString *lastName;
+@property (nonatomic, copy) NSString *fullName;
+
+@end
---
+++ Classes/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  PodDemo
+//
+//  Created by guojunxu on 2017/12/6.
+//  Copyright © 2017年 guojunxu. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
